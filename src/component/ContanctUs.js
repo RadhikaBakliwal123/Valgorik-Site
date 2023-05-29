48b69c9e6508aee810c1d20a700a65356d3f3780
RadhikaBakliwal123 <bakliwalradhika@gmail.com>
2023-05-29 17:03:34 +0530

    contact-us-created

--- src/component/ContanctUs.js
+++ src/component/ContanctUs.js
@@ -1,9 +1,33 @@
+import React from "react";
+import styled from "styled-components";
 const ContactUs = () => {
-    return (
-        <div className="container">
-          <h1>Contact Us</h1>
-          <p>Welcome to the Contanct Us page!</p>
+  const Wrapper = styled.section`
+  padding: 9rem 0 5rem 0`;
+    return <Wrapper>
+      <h2 className="common-heading">Contact Us</h2>
+      <div className="container">
+        <div className="contact-form">
+          <form action="https://formspree.io/f/mpzeebno" method="POST" className="contact-inputs">
+            <input 
+              type="text"
+              name="username"
+              placeholder="username"
+              autoComplete="off"
+              required
+              />
+            <input 
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+              />
+            <textarea name="message" cols="30" rows="6" autoComplete="off" required></textarea>
+
+            <input type="submit" value="send"></input>
+          </form>
         </div>
-      );
+      </div>
+    </Wrapper>;
 }
 export default ContactUs;
